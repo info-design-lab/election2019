6a4b290cb4564bcebb60d16f165309371b1b3336
Arihant <parsoyaarihant@gmail.com>
2019-02-05 18:11:51 +0530

    updated party colors and data

--- js/map.js
+++ js/map.js
@@ -9,17 +9,35 @@ queue()
 
 var map_tooltip = d3.select("#map")
     .append("div")
-    .attr('class', 'd3-tip');
+    .attr('class', 'd3-tip')
+    .attr("id", "map-tooltip");
+var map_tooltip_svg;
 
 var unknownColor = "#a8a8a8";
-var yearList = [1999, 2004, 2009, 2014];
+var yearList = [1999, 2004, 2009, 2014, 2019];
+var yearSlider = document.getElementById('year-slider');
+var year = 2014; // Current Year
+var map_legend;
+
+noUiSlider.create(yearSlider, {
+    start: [year],
+    step: 5,
+    range: {
+        'min': [1999],
+        'max': [2019]
+    },
+    pips: {
+        mode: 'values',
+        values: yearList,
+        density: 20
+    }
+});
 
 function makeMap(error, data, partyColors, map){
 	if(error){
 		console.log(error);
 	}
 
-    var year = 2014;
     var uncheckedParties = [];
     // Create Map
     var map_width = $('#map').width();
@@ -48,24 +66,10 @@ function makeMap(error, data, partyColors, map){
         .attr('stroke-width', '0.3px')
         .style('stroke', 'black')
         .on('mouseover', function(d){
-            d3.select(this).attr("stroke-width", "2px"); 
+            d3.select(this).attr("stroke-width", "2px");
             if(data[year][d.properties.PC_NAME.toUpperCase()]){
                 map_tooltip.style("visibility", "visible");
-                var tooltipText = '<strong>'+ d.properties.PC_NAME.toUpperCase() +'</strong> \
-                    <br><span style="font-size:17px">'+ data[year][d.properties.PC_NAME.toUpperCase()].Name +'</span>\
-                    <br><span style="font-size:15px; color:'+ partyColors[data[year][d.properties.PC_NAME.toUpperCase()].Party] +'">'+ data[year][d.properties.PC_NAME.toUpperCase()].Party+ '</span>    \
-                    <hr> \
-                    ';
-
-                for(var i in yearList){
-                    tooltipText = tooltipText + yearList[i] + 
-                    "&nbsp <span style='color: "+ partyColors[data[yearList[i]][d.properties.PC_NAME.toUpperCase()].Party] +"'>" + 
-                    data[yearList[i]][d.properties.PC_NAME.toUpperCase()].Party +
-                    "</span><br>";
-                }
-
-                map_tooltip.html(tooltipText);
-
+                createSimpleTooltip(d);
             }
         })
         .on("mousemove", function(d) {
@@ -77,7 +81,6 @@ function makeMap(error, data, partyColors, map){
             map_tooltip.style("visibility", "hidden");
         });
 
-    // Create Party legend
     var legend_width = $('#map-legend').width();
     var legend_height = 500;
     var legend_margin = {
@@ -87,104 +90,120 @@ function makeMap(error, data, partyColors, map){
     var legend_svg = d3.select("#map-legend").append("svg")
         .attr("width", legend_width)
         .attr("height", legend_height);
-    var legend = legend_svg.append('g')
-        .attr("transform", "translate("+ legend_margin.left +", "+ legend_margin.top +")");
-    /*
-    legend.append("text")
-        .attr("x", 10)
-        .attr('y', 10)
-        .style("font-size", "40")
-        .text("Summary")
-    */
-    var partiesData = getPartyList(data);  
-    var total = 0;
-    for(var i in partiesData){
-        total = total + partiesData[i][1];
-    }
-    var legendParties = legend.selectAll("circle")
-        .data(partiesData)
-        .enter();
 
-    legendParties.append("text")
-        .attr('x', 10)
-        .attr('y', function(d, i){
-            return 25*i + 70;
-        })
-        .style("font-size", "20")
-        .attr('fill', "#3f3f3f")
-        .text(function(d){
-            return d[0];
-        });
-    legendParties.append('circle')
-        .attr('cx', -5)
-        .attr("cy", function(d, i){
-            return 25*i + 63;
-        })
-        .attr('r', 10)
-        .attr('fill', function(d){
-            if(partyColors[d[0]]){
-                return partyColors[d[0]];
-            }
-            return unknownColor;
-        });
-    legendParties.append('circle')
-        .attr('cx', -5)
-        .attr("cy", function(d, i){
-            return 25*i + 63;
-        })
-        .attr('r', 8)
-        .attr('fill', "white");
-    legendParties.append('circle')
-        .attr('cx', -5)
-        .attr("cy", function(d, i){
-            return 25*i + 63;
-        })
-        .attr('r', 6)
-        .style("cursor", "pointer")
-        .attr('fill', function(d){
-            if(partyColors[d[0]]){
-                return partyColors[d[0]];
-            }
-            return unknownColor;
-        })
-        .on('click', function(d){
-            if(uncheckedParties.indexOf(d[0]) < 0){
-                uncheckedParties.push(d[0]);
-                d3.select(this).attr('fill', 'white');
-            }
-            else{
-                arrayRemoveElement(uncheckedParties, d[0]);
-                if(partyColors[d[0]]){
-                    d3.select(this).attr('fill', partyColors[d[0]]);
-                } else{
-                    d3.select(this).attr('fill', unknownColor);
-                }
-            }
-            updateMap();
-        });
-    legendParties.append("text")
-            .attr('x', -20)
+    map_legend = legend_svg.append('g')
+        .attr("transform", "translate("+ legend_margin.left +", "+ legend_margin.top +")");
+
+    yearSlider.noUiSlider.on('update', function(values, handle) {
+        year = parseInt(values);
+        updateMap();
+        createPartyLegend();
+    });
+
+    function createPartyLegend(){
+        // Create Party legend
+        /*
+        legend.append("text")
+            .attr("x", 10)
+            .attr('y', 10)
+            .style("font-size", "40")
+            .text("Summary")
+        */
+        map_legend.remove();
+        
+        map_legend = legend_svg.append('g')
+            .attr("transform", "translate("+ legend_margin.left +", "+ legend_margin.top +")");
+
+        var partiesData = getPartyList(data);  
+        var total = 0;
+        for(var i in partiesData){
+            total = total + partiesData[i][1];
+        }
+
+        var legendParties = map_legend.selectAll("circle")
+            .data(partiesData)
+            .enter();
+
+        legendParties.append("text")
+            .attr('x', 10)
             .attr('y', function(d, i){
                 return 25*i + 70;
             })
             .style("font-size", "20")
-            .style("text-anchor", "end")
+            .attr('fill', "#3f3f3f")
             .text(function(d){
-                return d[1];
+                return d[0];
+            });
+        legendParties.append('circle')
+            .attr('cx', -5)
+            .attr("cy", function(d, i){
+                return 25*i + 63;
             })
-    legend.append("text")
-        .attr('x', 10)
-        .attr("y", 30)
-        .style("font-size", "35")
-        .attr("fill", "#939393")
-        .text("Total Seats");
-    legend.append("text")
-        .attr('x', 0)
-        .attr("y", 30)
-        .style("font-size", "35")
-        .style("text-anchor", "end")
-        .text(total);
-
+            .attr('r', 10)
+            .attr('fill', function(d){
+                if(partyColors[d[0]]){
+                    return partyColors[d[0]];
+                }
+                return unknownColor;
+            });
+        legendParties.append('circle')
+            .attr('cx', -5)
+            .attr("cy", function(d, i){
+                return 25*i + 63;
+            })
+            .attr('r', 8)
+            .attr('fill', "white");
+        legendParties.append('circle')
+            .attr('cx', -5)
+            .attr("cy", function(d, i){
+                return 25*i + 63;
+            })
+            .attr('r', 6)
+            .style("cursor", "pointer")
+            .attr('fill', function(d){
+                if(partyColors[d[0]]){
+                    return partyColors[d[0]];
+                }
+                return unknownColor;
+            })
+            .on('click', function(d){
+                if(uncheckedParties.indexOf(d[0]) < 0){
+                    uncheckedParties.push(d[0]);
+                    d3.select(this).attr('fill', 'white');
+                }
+                else{
+                    arrayRemoveElement(uncheckedParties, d[0]);
+                    if(partyColors[d[0]]){
+                        d3.select(this).attr('fill', partyColors[d[0]]);
+                    } else{
+                        d3.select(this).attr('fill', unknownColor);
+                    }
+                }
+                updateMap();
+            });
+        legendParties.append("text")
+                .attr('x', -20)
+                .attr('y', function(d, i){
+                    return 25*i + 70;
+                })
+                .style("font-size", "20")
+                .style("text-anchor", "end")
+                .text(function(d){
+                    return d[1];
+                })
+        map_legend.append("text")
+            .attr('x', 10)
+            .attr("y", 30)
+            .style("font-size", "35")
+            .attr("fill", "#939393")
+            .text("Total Seats");
+        map_legend.append("text")
+            .attr('x', 0)
+            .attr("y", 30)
+            .style("font-size", "35")
+            .style("text-anchor", "end")
+            .text(total);    
+    }
     function getPartyList(d){
         var list = {};
         for(var i in d[year]){
@@ -213,7 +232,7 @@ function makeMap(error, data, partyColors, map){
         }
     }
     function updateMap(){
-        map.style('fill', function(d) {
+        map_svg.selectAll("path").transition().duration(500).style('fill', function(d) {
             if(data[year][d.properties.PC_NAME.toUpperCase()]){
                 if(uncheckedParties.indexOf(data[year][d.properties.PC_NAME.toUpperCase()].Party) > -1){
                     return "white";
@@ -221,6 +240,17 @@ function makeMap(error, data, partyColors, map){
                 return partyColors[data[year][d.properties.PC_NAME.toUpperCase()].Party];
             }
             return unknownColor;
-        })
+        });
+    }
+    function createSimpleTooltip(d){
+        if(map_tooltip_svg){
+            map_tooltip_svg.remove();
+        }
+        
+        map_tooltip_svg = d3.select("#map-tooltip").append("svg")
+        .attr("width", 300)
+        .attr("height", 200);
+
+        
     }
 }
